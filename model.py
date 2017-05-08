f88aa4b2a93de273461f71dec0973cbc0d246532
Samantha Nielsen <sknielsen88@gmail.com>
2017-05-08 22:38:02 +0000

    Add initial flask routes

--- model.py
+++ model.py
@@ -84,12 +84,13 @@ class Category(db.Model):
 ##############################################################################
 # Helper functions
 
+
 def connect_to_db(app):
     """Connect the database to our Flask app."""
 
-    # Configure to use our PostgreSQL database
+    # Configure to use our PstgreSQL database
     app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql:///travels'
-    app.config['SQLALCHEMY_ECHO'] = True
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
     db.app = app
     db.init_app(app)
 
@@ -98,9 +99,6 @@ if __name__ == "__main__":
     # As a convenience, if we run this module interactively, it will leave
     # you in a state of being able to work with the database directly.
 
-    # So that we can use Flask-SQLAlchemy, we'll make a Flask app
-    from flask import Flask
-    app = Flask(__name__)
-
+    from server import app
     connect_to_db(app)
     print "Connected to DB."
--- server.py
+++ server.py
@@ -2,10 +2,107 @@ from jinja2 import StrictUndefined
 from flask import (Flask, render_template, redirect, request, flash,
                    session)
 from model import User, Trip, Entry, Category, connect_to_db, db
-import os
 
 
 app = Flask(__name__)
 
 # Required to use Flask sessions
-app.secret_key = os.environ['SECRET_KEY']
+# app.secret_key = os.environ['SECRET_KEY']
+app.secret_key = 'abcde'
+
+app.jinja_env.undefined = StrictUndefined
+
+
+@app.route('/')
+def index():
+    """Homepage."""
+    return render_template("homepage.html")
+
+@app.route('/create-account', methods=['GET'])
+def create_account():
+    """ Allows user to create a new account """
+    return render_template('create_account.html')
+
+
+@app.route('/create-account', methods=['POST'])
+def check_create():
+    """ Checks user email is new and processes registration """
+
+    user_email = request.form.get('email')
+    user_password = request.form.get('password')
+    name = request.form.get('name')
+
+    user = User.query.filter_by(email=user_email).all()
+
+    if user:
+        flash("User email already exists")
+        return redirect('/login-form')
+    else:
+        new_user = User(email=user_email, password=user_password, name=name)
+        db.session.add(new_user)
+        db.session.commit()
+        session['logged_in_email'] = user_email
+        flash('You are now registered and logged in!')
+        return redirect('/users/%s' % (user.user_id))
+
+
+@app.route('/logout')
+def logout():
+    """Logs out user"""
+    del session['logged_in_email']
+    flash("You are now logged out.")
+    return redirect('/')
+
+
+@app.route('/login-form')
+def login():
+    """Prompts user to log in"""
+
+    return render_template('login_form.html')
+
+
+@app.route('/check-login', methods=['POST'])
+def check_login():
+    """Check if email in users table"""
+
+    user_email = request.form.get('email')
+    user_password = request.form.get('password')
+
+    try:
+        user = User.query.filter_by(email=user_email).one()
+        if user.password == user_password:
+            session['logged_in_email'] = user_email
+            flash('Hello, %s' % (user.name))
+            return redirect('/users/%s' % (user.user_id))
+        else:
+            flash('Wrong password!')
+            return redirect('/login-form')
+
+    except:
+        flash("No user with that email")
+        return redirect('/create-account')
+
+
+@app.route('/users/<user_id>')
+def user_profile(user_id):
+    """Displays a user's profile """
+    user = User.query.get(user_id)
+
+    user_trips = user.trips
+    # user_ratings = []
+    # for rating in ratings:
+    #     user_ratings.append((rating.movie.title, rating.score))
+
+    # return render_template('user_profile.html', user_age=user_age, user_zip=user_zip,
+                            # user_ratings=user_ratings)
+    return render_template('user_profile.html', trips=user_trips)
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    # app.debug = True
+    app.jinja_env.auto_reload = app.debug  # make sure templates, etc. are not cached in debug mode
+
+    connect_to_db(app)
+    app.run(port=5000, host='0.0.0.0')
