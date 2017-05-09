201446c663dda059c7e4a67c2f0271b858d5babf
Samantha Nielsen <sknielsen88@gmail.com>
2017-05-09 00:41:30 +0000

    Add categories seed data

---
+++ seed.py
@@ -0,0 +1,33 @@
+from model import Category
+from model import connect_to_db, db
+from server import app
+
+
+def load_categories():
+    """Load categories from category.txt into database."""
+
+    # Delete all rows in table, so if we need to run this a second time,
+    # we won't be trying to add duplicate users
+    Category.query.delete()
+
+    # Read u.user file and insert data
+    for row in open("category.txt"):
+        row = row.rstrip()
+        name = row
+
+        category = Category(name=name)
+
+        # We need to add to the session or it won't ever be stored
+        db.session.add(category)
+
+    # Once we're done, we should commit our work
+    db.session.commit()
+
+if __name__ == "__main__":
+    connect_to_db(app)
+
+    # In case tables haven't been created, create them
+    db.create_all()
+
+    # Import different types of data
+    load_categories()
